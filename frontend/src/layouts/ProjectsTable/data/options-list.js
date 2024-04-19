9cc644fae7bbe82f5b9a3afa3a8395a9890d7cd5
Mibanana <mibanana@mibanana.com>
2024-04-19 11:01:27 +0500

    edit modal changes

--- frontend/src/layouts/ProjectsTable/data/options-list.js
+++ frontend/src/layouts/ProjectsTable/data/options-list.js
@@ -34,6 +34,7 @@ const CustomerList = (props) => {
     }
     return (
         <React.Fragment>
+        <MenuItemDropdown loading={false} disabled={false} onClick={()=>{}} title="Edit" />
         <MenuItemDropdown loading={loading3} disabled={loading3 || getActionsDisabled()} onClick={projectCompleted} title="Completed" />
         <MenuItemDropdown loading={loading2} disabled={loading2} onClick={duplicateProject} title="Duplicate" />
             <MenuItemDropdown loading={loading1} disabled={loading1} onClick={projectCancel} title="Cancel" />
